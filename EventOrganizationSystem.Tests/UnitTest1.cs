c86837c984f7d3832df9a25b0b0ef8a4e7ceb780
ayhamdw <s12029113@stu.najah.edu>
2025-06-21 19:21:26 +0300

    008-1: Restructure folders, add test project

---
+++ EventOrganizationSystem.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EventOrganizationSystem.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(4 , 2 + 2);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        Assert.NotEqual(5, 2 + 2);
+    }
+}
